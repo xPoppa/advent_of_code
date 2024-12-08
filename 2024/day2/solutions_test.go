e7a480d43b3a407fbccd3ba7d679706a92cdcf47
Romano Lynch <lynch@floydhamilton.nl>
2024-12-08 19:26:28 +0100

    Add tests for debugging solution

---
+++ 2024/day2/solutions_test.go
@@ -0,0 +1,18 @@
+package day2_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/xPoppa/aoc/2024/day2"
+)
+
+func TestIsSafe(t *testing.T) {
+	home := os.Getenv("HOME")
+	input := home + "/go/advent_of_code/2024/day2/test.txt"
+	res := day2.Part1(input)
+
+	if res != 2 {
+		t.Fatal("Didn't get the right amount should be 2 but is: ", res)
+	}
+}
